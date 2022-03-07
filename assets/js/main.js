36a6ef943586a37d2b5fbc5ff072902a3ef7ddfb
notharshprajapati <prajapati.harsh0243@gmail.com>
2022-03-07 13:24:35 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalClose = document.querySelectorAll('.services__modal-close');
+
+let modal = function (modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc, i) => {
+    mc.addEventListener('click', () => {
+        modalViews.forEach((mv) => {
+            mv.classList.remove('active-modal')
+        })
+    })
+})
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+/* Link active work */
+const linkwork = document.querySelectorAll('.work__item')
+
+function activeWork(){
+    linkwork.forEach(l=> l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+linkwork.forEach(l=> l.addEventListener('click',activeWork))
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== LIGHT DARK THEME ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
